1272da8288f07384d126e2ba704dbaec3540975d
Diogo <diogocerqueiradelima@gmail.com>
2025-06-14 15:49:16 +0100

    build: add spring boot

--- authorization-server/build.gradle.kts
+++ authorization-server/build.gradle.kts
@@ -1,15 +1,22 @@
 plugins {
     id("java")
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
 
 group = "com.github.diogocerqueiralima"
-version = "unspecified"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+
+    implementation(libs.spring.boot.starter)
+    implementation(libs.spring.boot.starter.web)
+
+    testImplementation(libs.spring.boot.starter.test)
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ authorization-server/src/test/java/com/github/diogocerqueiralima/authorizationserver/AuthorizationServerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.github.diogocerqueiralima.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTest {
+
+    @Test
+    void contextLoads() {}
+
+}
