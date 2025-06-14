a39bedb651dba5cb41260e7f4bbad0a864a9b580
Diogo <diogocerqueiradelima@gmail.com>
2025-06-14 17:57:59 +0100

    build: add postgresql and validation

--- authorization-server/build.gradle.kts
+++ authorization-server/build.gradle.kts
@@ -13,8 +13,12 @@ repositories {
 
 dependencies {
 
+    implementation(libs.postgresql)
     implementation(libs.spring.boot.starter)
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.spring.boot.starter.oauth2.authorization.server)
 
     testImplementation(libs.spring.boot.starter.test)
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
